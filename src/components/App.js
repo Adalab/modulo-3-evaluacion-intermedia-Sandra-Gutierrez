bc10b11f591be7c12ea026254b6f953e37e0a609
Sandra Gutierrez <sandsan12@gmail.com>
2022-01-18 10:37:33 +0100

    Fetch component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,39 @@
 import '../styles/App.scss';
 import { useEffect, useState } from 'react';
-import adalabers from '../services/data.json';
-//import callToApi from '../services/api';
+import callToApi from '../services/api';
 
 function App() {
   // Estado
-  const data = adalabers;
-  console.log(data.results);
+  const [data, setData] = useState([]);
   // Variables
   // Eventos
+  const handeFormSubmit = (ev) =>{
+    ev.preventDefault();
+  }
+  const handelInputText = (ev) =>{
+    const valueInput = ev.currentTarget.value;
+    const newAdalaber = {
+      name: valueInput,
+      counselor: valueInput,
+      speciality: valueInput,
+    }
+  }
+
+  const handelBtnAddAdalaber = () => {
+
+  }
 
   // api
-/*   useEffect(() => {
+  useEffect(() => {
     callToApi().then((response) => {
       setData(response);
     });
-  }, []); */
+  }, []); 
 
   // Render
   const renderAdalabers = () => {
-    return data.results.map( adalaber => {
-      return <tr>
+    return data.map( adalaber => {
+      return <tr key={adalaber.id}>
       <td>{adalaber.name}</td>
       <td>{adalaber.counselor}</td>
       <td>{adalaber.speciality}</td>
@@ -28,7 +41,7 @@ function App() {
     })
   }
   
-
+  // Return HTML
   return (
     <div>
       <h1>Adalabers</h1>
@@ -42,6 +55,16 @@ function App() {
         </thead>
         <tbody>{renderAdalabers()}</tbody>
       </table>
+      <h2>Añadir nueva Adalaber</h2>
+      <form action="" onSubmit={handeFormSubmit}>
+        <label htmlFor="name">Nombre</label>
+        <input type="text" placeholder="Ej: Sandra" onChange={handelInputText}/>
+        <label htmlFor="name">Tutora</label>
+        <input type="text" placeholder="Ej: Dayana" onChange={handelInputText}/>
+        <label htmlFor="name">Especialidad</label>
+        <input type="text" placeholder="Ej: JavaScript" onChange={handelInputText}/>
+        <button onClick={handelBtnAddAdalaber}>Añadir</button>
+      </form>
     </div>
 
   );
--- src/services/api.js
+++ src/services/api.js
@@ -2,14 +2,7 @@ const callToApi = () => {
     return fetch('https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json')
       .then((response) => response.json())
       .then((response) => {
-        const result = {
-          id: response.results.id,
-          name: response.results.name,
-          counselor: response.results.counselor,
-          promo: response.results.promo,
-          speciality: response.results.speciality,
-        };
-        return result;
+        return response.results;
       });
   };
   
